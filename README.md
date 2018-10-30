d231b41b7091a9ded414dab73b148c7dc628a704
Théo DELECOUR <theo.delecour@gmx.com>
2018-10-30 09:44:52 +0100

    complete setup.py, remove requirements.txt

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # powerspot
 
 Command line interface in Python for automated operations with Spotify, based on the [click](https://github.com/pallets/click) package.
--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup
+import os
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
 
 setup(
     name='powerspot',
     version='0.1',
+    description='CLI for automated operations with Spotify',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6.0',
+    url='https://github.com/theolamayo/powerspot',
     py_modules=['powerspot'],
-    install_requires=[
-        'Click',
-    ],
     entry_points='''
         [console_scripts]
         powerspot=main:main
     ''',
+    install_requires=[
+        'Click',
+        'spotipy',
+        'tabulate',
+    ],
+    licence='MIT',
 )
